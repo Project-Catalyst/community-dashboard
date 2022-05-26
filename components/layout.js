dd641d5dd247f233cea41af72f5461779140c344
Plamen Petkov <plamen.krasimirov.petkov@gmail.com>
2022-05-26 19:01:24 +0300

    add pie chart component

---
+++ components/layout.js
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+
+const name = 'Plamen Petkov';
+export const siteTitle = 'Community Dashboard';
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="community dashboard showing CA and vCA progress and stats"
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
