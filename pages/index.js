4b2f3ea6391bdc1593a97165637572e7b9fcaec6
Plamen Petkov <plamen.krasimirov.petkov@gmail.com>
2022-06-03 15:16:39 +0300

    clean up

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,14 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import AnimatedChart from '../components/animated-chart';
 import utilStyles from '../styles/utils.module.css';
-import { getChartDataFromJson, getChartOptions } from '../lib/chart-data-logic';
+import { getChartDataFromJson } from '../lib/chart-data-logic';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import * as React from 'react';
 import Slider from '@mui/material/Slider';
 import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
-
-import Link from 'next/link';
-import { stepConnectorClasses } from '@mui/material';
 
 export async function getStaticProps() {
   const data = await getChartDataFromJson()
@@ -25,9 +21,6 @@ export async function getStaticProps() {
 }
 
 function Home(props) {
-
-
-  //set based on dropdown
   const [fundNumber, setFundNumber] = React.useState(0);
 
   const [boundProps, setBoundProps] = useState(props.fundsData[0].fundData);
@@ -41,7 +34,7 @@ function Home(props) {
   const handleDropdownChange = (event) => {
     const chosenFundIndex = event.target.value
     setFundNumber(chosenFundIndex);
-    setAnimation(props.fundsData[chosenFundIndex].fundData.animationData);
+    setAnimation(props.fundsData[chosenFundIndex].fundData.animationData)
   };
 
   const handleSliderChange = (_, newValue) => {
@@ -63,8 +56,7 @@ function Home(props) {
             id="demo-simple-select"
             value={fundNumber}
             label="Fund"
-            onChange={handleDropdownChange}
-          >
+            onChange={handleDropdownChange}>
             {menuItems}
           </Select>
         </FormControl>
@@ -75,13 +67,13 @@ function Home(props) {
       </section>
       <section>
         <Slider
-          aria-label="Temperature"
+          aria-label="chartData"
           defaultValue={0}
           valueLabelDisplay="off"
           onChange={handleSliderChange}
           step={1}
-          marks
           min={0}
+          marks
           max={animation.length - 1}
         />
       </section>
